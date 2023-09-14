b42763fe9671745019c60b637d365e7928137d80
paulagarcia05 <paulagarciagodos@gmail.com>
2023-09-14 17:01:09 +0200

    Update dec2bin.py

--- dec2bin.py
+++ dec2bin.py
@@ -7,7 +7,7 @@
 # ------------------------------------------------------
 def dec2bin(numero_decimal, numero_bits):
     numero_binario = bin(numero_decimal)
-    if numero_decimal <= 0
+    if numero_decimal <= 0:
         numero_binario = numero_binario[2:len(numero_binario)]
         while len(numero_binario) < numero_bits:  
             numero_binario = "0" + numero_binario
